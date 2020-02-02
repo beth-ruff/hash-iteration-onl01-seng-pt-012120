9ae45b0b7a97218d3db444e11dd628794f62d312
Beth Ruff <beth.ruff2@gmail.com>
2020-02-02 07:31:43 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,9 +12,8 @@ end
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    put "only prints the birthday greeting if the birthday kid 12 or younger" do
-    birthday_kids = {
-      "Timmy" => 9,
-      "Sarah" => 6,
-      "Amanda" => 27
-    }
+    if age < 12
+      puts "Happy birthday #{kids_name}! You are now #{age} years old!"
+    end
+  end
+end
